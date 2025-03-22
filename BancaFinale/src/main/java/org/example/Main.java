0e598a1b0ea4ee5cd746abbd25f91cff613c20b7
Sve 😬😬😬 <sventibusiness@gmail.com>
2025-03-23 00:09:24 +0100

    Add files via upload

---
+++ BancaFinale/src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Start the login window
+        SwingUtilities.invokeLater(() -> {
+            LoginFrame loginFrame = new LoginFrame();
+            loginFrame.setVisible(true);
+        });
+    }
+}
---
+++ BancaFinale/src/main/java/org/example/UserAccount.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import java.io.Serializable;
+
+public class UserAccount implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String username;
+    private String password;
+    private double portafoglio;
+    private double contoBancario;
+    private int anno;
+    private int mesiTrascorsi;
+
+    public UserAccount(String username, String password) {
+        this.username = username;
+        this.password = String.valueOf(password);
+        this.portafoglio = 100.0; // Initial funds
+        this.contoBancario = 0.0;
+        this.anno = 2024;
+        this.mesiTrascorsi = 0;
+    }
+
+    public UserAccount(String testUser, double v, double v1, int i, int i1) {
+    }
+
+    // Getters and setters
+    public String getUsername() { return username; }
+    public boolean checkPassword(String password) { return this.password.equals(password); }
+    public double getPortafoglio() { return portafoglio; }
+    public void setPortafoglio(double portafoglio) { this.portafoglio = portafoglio; }
+    public double getContoBancario() { return contoBancario; }
+    public void setContoBancario(double contoBancario) { this.contoBancario = contoBancario; }
+    public int getAnno() { return anno; }
+    public void setAnno(int anno) { this.anno = anno; }
+    public int getMesiTrascorsi() { return mesiTrascorsi; }
+    public void setMesiTrascorsi(int mesiTrascorsi) { this.mesiTrascorsi = mesiTrascorsi; }
+
+    public void advanceMonth() {
+        mesiTrascorsi++;
+        // Add 100€ to the wallet every month
+        portafoglio += 100.0;
+    }
+}
